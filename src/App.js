786548eec8e5b06c94d57f1fe60fbf9495240dee
Fikasentani <mpilo.dlamini@yahoo.com>
2021-05-08 18:05:35 +0200

    addding ability to add to store and connecting redux devtools

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import People from "./components/People";
+import Input from "./components/Input";
 
 function App() {
   return (
     <div className="App">
       Yaw Yaw
+      <Input />
       <People />
     </div>
   );
--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import create from "zustand";
+import { devtools } from "zustand/middleware";
 
-const useStore = create((set) => ({
+const store = (set) => ({
   people: ["Mpillz", "Dlamz"],
   addPerson: (person) =>
     set((state) => ({ people: [...state.people, person] })),
-}));
+});
+const useStore = create(devtools(store));
 
 export default useStore;
